26b5d608e3e58ee885af9b048fefd887196d93d6
Alysson Victor <developeralysson@gmail.com>
2021-11-10 21:26:27 -0300

    Se aplicado, esse commit vai adicionar o teste passando (valor da fatura for menor que 2000)

--- src/test/filtrarfaturas.test.js
+++ src/test/filtrarfaturas.test.js
@@ -5,7 +5,7 @@ const filtrarfaturas = require("../utils/filtrarfaturas.js");
 describe("Filtrar Faturas", () => {
 	test("Valor menor que 2000", () => {
 		let cliente1 = new Cliente("Alysson Victor", "2021-11-10", "PR");
-        let fatura1 = new Fatura("0001", 500, "2021-11-10", cliente1);
+        let fatura1 = new Fatura("0001", 2000, "2021-11-10", cliente1);
 
         let cliente2 = new Cliente("Pedro Paulo", "2021-11-09", "SP");
         let fatura2 = new Fatura("0002", 1500, "2021-11-10", cliente2);
@@ -13,6 +13,6 @@ describe("Filtrar Faturas", () => {
 
         let faturasFake = new Array(fatura1, fatura2, fatura3);
 
-		expect(filtrarfaturas(faturasFake)).toBeDefined();
+		expect(filtrarfaturas(faturasFake)).not.toContain(fatura2);
 	});
 });
--- src/utils/filtrarfaturas.js
+++ src/utils/filtrarfaturas.js
@@ -1,7 +1,8 @@
 module.exports = (faturas) => {
     for(let i = 0; i < faturas.length; i++){
         if(faturas[i].getValor() < 2000){
-            return undefined;
+            faturas.splice(1,1);
+            return faturas;
         }
     }
 }
