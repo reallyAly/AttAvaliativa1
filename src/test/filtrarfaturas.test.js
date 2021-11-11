2e44c0abb2d9debfd3e354b94cbde0b960399be5
Alysson Victor <developeralysson@gmail.com>
2021-11-10 22:10:07 -0300

    Se aplicado, esse commit vai adicionar o teste falhando (Se o valor da fatura estiver entre 2000 e 2500 e a data for menor ou igual a de um mês)

--- src/test/filtrarfaturas.test.js
+++ src/test/filtrarfaturas.test.js
@@ -2,17 +2,22 @@ const Cliente = require("../cliente.js");
 const Fatura = require("../fatura.js");
 const filtrarfaturas = require("../utils/filtrarfaturas.js");
 
+let cliente1 = new Cliente("Alysson Victor", new Date(2021,11,10), "PR");
+let fatura1 = new Fatura("0001", 2000, new Date(2021,11,10), cliente1);
+let cliente2 = new Cliente("Pedro Paulo", new Date(2021,11,9), "SP");
+let fatura2 = new Fatura("0002", 1500, new Date(2021,11,10), cliente2);
+let fatura3 = new Fatura("0003", 6000, new Date(2021,11,5), cliente1);
+
+let faturasFake = new Array(fatura1, fatura2, fatura3);
+
 describe("Filtrar Faturas", () => {
+
 	test("Valor menor que 2000", () => {
-		let cliente1 = new Cliente("Alysson Victor", "2021-11-10", "PR");
-        let fatura1 = new Fatura("0001", 2000, "2021-11-10", cliente1);
-
-        let cliente2 = new Cliente("Pedro Paulo", "2021-11-09", "SP");
-        let fatura2 = new Fatura("0002", 1500, "2021-11-10", cliente2);
-        let fatura3 = new Fatura("0003", 6000, "2021-11-05", cliente1);
-
-        let faturasFake = new Array(fatura1, fatura2, fatura3);
-
 		expect(filtrarfaturas(faturasFake)).not.toContain(fatura2);
 	});
+
+    test("Se o valor da fatura estiver entre 2000 e 2500 e a data for menor ou igual a de um mês atrás", () => {
+		expect(filtrarfaturas(faturasFake)).toBeDefined();
+	});
+
 });
--- src/utils/filtrarfaturas.js
+++ src/utils/filtrarfaturas.js
@@ -1,8 +1,10 @@
 module.exports = (faturas) => {
     for(let i = 0; i < faturas.length; i++){
-        if(faturas[i].getValor() < 2000){
+        if( faturas[i].getValor() < 2000 ){
             faturas.splice(i,1);
             return faturas;
         }
     }
+
+    return undefined;
 }
