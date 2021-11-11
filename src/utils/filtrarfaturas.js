fc58fffd09eda53e80cb3fec5b61c26adc0f9065
Alysson Victor <developeralysson@gmail.com>
2021-11-10 21:29:49 -0300

    Se aplicado, esse commit vai adicionar o teste refatorado (valor da fatura for menor que 2000)

--- src/utils/filtrarfaturas.js
+++ src/utils/filtrarfaturas.js
@@ -1,10 +1,8 @@
 module.exports = (faturas) => {
     for(let i = 0; i < faturas.length; i++){
         if(faturas[i].getValor() < 2000){
-            faturas.splice(1,1);
+            faturas.splice(i,1);
             return faturas;
         }
     }
 }
-
-
